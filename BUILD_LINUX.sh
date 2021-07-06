515bd2b6e5fc51db15304847933fbbc6c949b860
Mike Weiblen <mew@mew.cx>
2021-07-06 02:54:02 -0600

    BUILD_LINUX.sh : add comments, actually build the exe

--- BUILD_LINUX.sh
+++ BUILD_LINUX.sh
@@ -1,15 +1,22 @@
 #! /bin/bash -x
 
+# build the source directory
 make clean
 make release
 
+# clean up the source directory
 cd release/
 rm -rf sen44-*
 rm sps30-uart-*.zip
 rm sps30-uart
 mv sps30-uart-* sps30-uart
 
+# build the executable
 cd sps30-uart/
 mv sample-implementations/linux/sensirion_uart_implementation.c .
 rm -rf sample-implementations/ test_projects/
-#make
+make
+
+echo "The executable can be found at release/sps30-uart/sps30_example_usage"
+
+# vim: set sw=4 ts=8 et ic ai:
